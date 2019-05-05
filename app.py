3f1eaf82721cae9c8273486cee167e1d072ec8c2
陳勝舢 <54sakuramiku@gmail.com>
2019-05-05 15:13:03 +0800

    debug text

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message - TextSendMessage(text='沫兒收到您的回覆囉!')
+    message = TextSendMessage(text='沫兒收到您的回覆囉!')
     # message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
