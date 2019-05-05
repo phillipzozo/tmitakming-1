2dfa68335a13eb98ae58036978b8ad976ee29d4c
陳勝舢 <54sakuramiku@gmail.com>
2019-05-05 18:22:17 +0800

    debug text2

--- cusFirebase.py
+++ cusFirebase.py
@@ -13,18 +13,15 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 #Get Collection
 doc_ref = db.collection('Detector').document('gas')
-# for doc in docs:
-    # print(u'{} => {}'.format(doc.id, doc.to_dict()))
 localtime = time.asctime(time.localtime(time.time()))
 temp = '最後監測時間為 =>'+ localtime + '\n' 
 try:
     doc = doc_ref.get()
-    # 透過 to_dict()將文件轉為dictionary
+# 透過 to_dict()將文件轉為dictionary
     temp +=  'PM2.5 => {}'.format(doc.to_dict()['PM2.5']) + '\n' + '二氧化碳 => {}'.format(doc.to_dict()['CO2']) + '\n' + '酒精 => {}'.format(doc.to_dict()['Ethanol']) + '\n' + '一氧化碳 => {}'.format(doc.to_dict()['CO']) + '\n'
-    # print("文件內容為：{}".format(doc.to_dict()))
-    # print(temp)
+# print("文件內容為：{}".format(doc.to_dict()))
+# print(temp)
 except:
-    # print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
+# print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
 #print(temp)
 
-# print('PM2.5 => {}'.format(doc.to_dict()['PM2.5']))
