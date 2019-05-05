e945f3c8e65533cd8fff9af2ef8e6f36f33f2e6e
陳勝舢 <54sakuramiku@gmail.com>
2019-05-05 16:51:37 +0800

    debug text2

--- app.py
+++ app.py
@@ -42,8 +42,10 @@ def handle_message(event):
     if re.match('沫兒',event.message.text):
         MoReply = True #沫兒確認匹配成功
     message = TextSendMessage(text='沫兒收到您的回覆囉!')
-    if re.search('我帥嗎',event.message.text):
-        message = TextSendMessage(text='沫兒覺得勝舢最帥了!')
+    if re.search('華哥帥嗎',event.message.text):
+        message = TextSendMessage(text='沫兒覺得華哥最帥了!')
+    if re.search('再見',event.message.text):
+        message = TextSendMessage(text='勝舢哥掰掰!')   
     # message = TextSendMessage(text=event.message.text)
     if MoReply == True:
         line_bot_api.reply_message(event.reply_token, message)
