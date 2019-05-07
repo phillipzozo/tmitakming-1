7570ed7177f6dc71dccfe377dd32be07113ba1d9
陳勝舢 <54sakuramiku@gmail.com>
2019-05-07 22:03:45 +0800

    add code

--- app.py
+++ app.py
@@ -32,25 +32,78 @@ cred = credentials.Certificate('serviceAccount.json')
 # 初始化firebase，注意不能重複初始化
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+#首次上傳時初始化用
+path_root = "Detector"
+collection_ref = db.collection(path_root)
+docs = collection_ref.get()
+cc = 0
+for doc in docs:
+    #print("文件內容：{}".format(doc.to_dict()))
+    cc += 1  # 取得最後一筆流水號
+#print(cc)
+#path = "Detector/gas"+str(cc) #現版本被禁用 暫時忽略
+#doc_ref = db.document(path)
+temp = ''
+doc_ref = db.collection('Detector').document('gas')
+
+
+def query_resetSearch():
+    path_root = "Detector"
+    collection_ref = db.collection(path_root)
+    docs = collection_ref.get()
+    cc = 0
+    for docx in docs:
+        #print("文件內容：{}".format(doc.to_dict()))
+        cc += 1  # 取得最後一筆流水號
+    #print(cc)
+    #path = "Detector/gas"+str(cc)
+    #doc_ref = db.document(path)
+    path = "gas"+str(cc)
+    return db.collection('Detector').document(path)
+
+
 def query_Get():
     # 初始化firestore
-    
+    doc_ref = query_resetSearch()
     #Get Collection
-    doc_ref = db.collection('Detector').document('gas')
-    localtime = time.asctime(time.localtime(time.time()))
-    temp = '監測時間 => '+ localtime + '\n' 
+    global temp
+    temp = ''
     try:
         doc = doc_ref.get()
         # 透過 to_dict()將文件轉為dictionary
-        temp +=  'PM2.5 => {}'.format(doc.to_dict()['PM2.5']) + '\n' + '二氧化碳 => {}'.format(doc.to_dict()['CO2']) + '\n' + '酒精 => {}'.format(doc.to_dict()['Ethanol']) + '\n' + '一氧化碳 => {}'.format(doc.to_dict()['CO']) + '\n'
+        temp += 'id => {}'.format(doc.to_dict()['id']) + '\n' + 'PM2.5 => {}'.format(doc.to_dict()['PM2.5']) + '\n' + '二氧化碳 => {}'.format(doc.to_dict()['CO2']) + '\n' + '酒精 => {}'.format(doc.to_dict()['Ethanol']) + '\n' + '一氧化碳 => {}'.format(
+            doc.to_dict()['CO']) + '\n' + '溫度 => {}'.format(doc.to_dict()['temperture']) + '\n' + '濕度 => {}'.format(doc.to_dict()['humidity']) + '\n' + '時間戳 => {}'.format(doc.to_dict()['timestamp']) + '\n'
         # print("文件內容為：{}".format(doc.to_dict()))
-        # print(temp)
+        #print(temp)
+       # print(doc_ref)
+      #  print(doc)
     except:
-        print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
-        # print(temp)
+        temp = '發生意外錯誤'
+       # print(temp)
 
-#def query_insert():
-    
+
+def queryInsert(insertStr):
+    query_resetSearch()
+    tempstr = 'gas'+str(cc)
+    doc_ref = db.collection("Detector").document(tempstr)
+    doc_ref.set(insertStr)
+
+#try:
+ #   doc = doc_ref.get()
+    # 透過 to_dict()將文件轉為dictionary
+ #   print("文件內容為：{}".format(doc.to_dict()))
+#except:
+  #  print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
+
+
+test = []
+cc += 1  # 下一個流水號
+
+test = {'id': cc, 'CO2': 1100, 'Ethanol': 0.4, 'CO': 200, 'id': 1, 'PM2.5': 299,
+        'temperture': 30, 'humidity': 50, 'timestamp': (a+o).strftime("%Y-%m-%d %H:%M:%S")}
+#queryInsert(test)
+#query_Get()
 
 
 
@@ -106,7 +159,11 @@ def handle_message(event):
     if re.search('我帥嗎',event.message.text):
         message = TextSendMessage(text='沫兒覺得勝舢最帥了!')
     if re.search('回報車內狀況',event.message.text):
+        query_Get()
         message = TextSendMessage(text=temp)
+    if re.search('新增資料',event.message.text):
+        queryInsert(test)
+        message = TextSendMessage(text='新增成功') #未來加入感測器失效錯誤回報
     # message = TextSendMessage(text=event.message.text)
     if re.search('profile_user',event.message.text):
         user_id = event.source.user_id
--- test _firebase.py
+++ test _firebase.py
@@ -5,7 +5,9 @@ import time
 import datetime
 import schedule
 
-
+a = datetime.datetime.today()
+o = datetime.timedelta(hours=8)
+#print((a+o).strftime("%Y-%m-%d_%H:%M"))
 cred = credentials.Certificate('serviceAccount.json')
 
 # 初始化firebase，注意不能重複初始化
@@ -13,40 +15,73 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-def query_Get():
-    # 初始化firestore
 
-    
-    #Get Collection
-    doc_ref = db.collection('Detector')
-    localtime = time.asctime(time.localtime(time.time()))
-    temp = '監測時間 => ' + localtime + '\n'
-    try:
-        doc = doc_ref.get()
-        # 透過 to_dict()將文件轉為dictionary
-        #temp += 'PM2.5 => {}'.format(doc.to_dict()['PM2.5']) + '\n' + '二氧化碳 => {}'.format(doc.to_dict()[
-        #    'CO2']) + '\n' + '酒精 => {}'.format(doc.to_dict()['Ethanol']) + '\n' + '一氧化碳 => {}'.format(doc.to_dict()['CO']) + '\n'
-        # print("文件內容為：{}".format(doc.to_dict()))
-        
-        print(doc_ref)
-        print(doc)
-    except:
-        print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
-        # print(temp)
 
 #query_Get()
 
 
-path = "Detector/gas"
+
+#首次上傳時初始化用
 path_root = "Detector"
-doc_ref = db.document(path)
 collection_ref = db.collection(path_root)
 docs = collection_ref.get()
+cc = 0
 for doc in docs:
-    print("文件內容：{}".format(doc.to_dict()))
-try:
-    doc = doc_ref.get()
+    #print("文件內容：{}".format(doc.to_dict()))
+    cc+=1 #取得最後一筆流水號
+#print(cc)
+#path = "Detector/gas"+str(cc) #現版本被禁用 暫時忽略
+#doc_ref = db.document(path)
+temp = ''
+doc_ref = db.collection('Detector').document('gas')
+def query_resetSearch():
+    path_root = "Detector"
+    collection_ref = db.collection(path_root)
+    docs = collection_ref.get()
+    cc = 0
+    for docx in docs:
+        #print("文件內容：{}".format(doc.to_dict()))
+        cc += 1  # 取得最後一筆流水號
+    #print(cc)
+    #path = "Detector/gas"+str(cc)
+    #doc_ref = db.document(path)
+    path = "gas"+str(cc)
+    return db.collection('Detector').document(path)
+
+def query_Get():
+    # 初始化firestore
+    doc_ref = query_resetSearch()
+    #Get Collection
+    global temp
+    try:
+        doc = doc_ref.get()
+        # 透過 to_dict()將文件轉為dictionary
+        temp += 'id => {}'.format(doc.to_dict()['id'])+ '\n' +'PM2.5 => {}'.format(doc.to_dict()['PM2.5']) + '\n' + '二氧化碳 => {}'.format(doc.to_dict()['CO2']) + '\n' + '酒精 => {}'.format(doc.to_dict()['Ethanol']) + '\n' + '一氧化碳 => {}'.format(doc.to_dict()['CO']) + '\n' + '溫度 => {}'.format(doc.to_dict()['temperture']) + '\n' + '濕度 => {}'.format(doc.to_dict()['humidity']) + '\n'  + '時間戳 => {}'.format(doc.to_dict()['timestamp']) + '\n'
+        # print("文件內容為：{}".format(doc.to_dict()))
+        #print(temp)
+       # print(doc_ref)
+      #  print(doc)
+    except:
+        temp += '意外錯誤'
+       # print(temp)
+
+def queryInsert(insertStr):
+    query_resetSearch()
+    tempstr = 'gas'+str(cc)
+    doc_ref = db.collection("Detector").document(tempstr)
+    doc_ref.set(insertStr)
+
+#try:
+ #   doc = doc_ref.get()
     # 透過 to_dict()將文件轉為dictionary
-    print("文件內容為：{}".format(doc.to_dict()))
-except:
-    print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
+ #   print("文件內容為：{}".format(doc.to_dict()))
+#except:
+  #  print("指定文件的路徑{}不存在，請檢查路徑是否正確".format(path))
+
+test = []
+cc+=1 #下一個流水號
+
+test = {'id': cc, 'CO2': 1100, 'Ethanol': 0.4, 'CO': 200, 'id': 1, 'PM2.5': 299,
+        'temperture': 30, 'humidity': 50, 'timestamp': (a+o).strftime("%Y-%m-%d %H:%M:%S")}
+queryInsert(test)
+#query_Get()
