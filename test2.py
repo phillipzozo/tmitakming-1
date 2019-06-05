a7ed38b3f0b8da3a60d40e0e7b31dbe08c449503
陳勝舢 <54sakuramiku@gmail.com>
2019-06-05 20:35:26 +0800

    Add code

---
+++ test2.py
@@ -0,0 +1,43 @@
+import time
+import threading
+
+
+def setInterval(interval, times=-1):
+    # This will be the actual decorator,
+    # with fixed interval and times parameter
+    def outer_wrap(function):
+        # This will be the function to be
+        # called
+        def wrap(*args, **kwargs):
+            stop = threading.Event()
+
+            # This is another function to be executed
+            # in a different thread to simulate setInterval
+            def inner_wrap():
+                i = 0
+                while i != times and not stop.isSet():
+                    stop.wait(interval)
+                    function(*args, **kwargs)
+                    i += 1
+
+            t = threading.Timer(0, inner_wrap)
+            t.daemon = True
+            t.start()
+            return stop
+        return wrap
+    return outer_wrap
+
+
+
+
+
+@setInterval(5)
+def foo(a):
+    print(a)
+
+
+foo('bar')
+
+# time.sleep(5)
+# stopper.set()
+# It will stop here, after printing 'bar' 5 times.
